4fb937da59669e27e05cf3749cb2268ad777daea
theodorepgl <limlimtheo96@gmail.com>
2023-09-01 15:49:13 +0800

    figuring out

--- rubyprogram.rb
+++ rubyprogram.rb
@@ -1,1 +1,12 @@
 # Write a Ruby program that takes a sentence as input and outputs the number of words in it.
+
+def count_words(sentence)
+  words = sentence.split(" ")
+  words.count
+end
+
+print "Enter a sentence: "
+sentence = gets.chomp
+
+word_count = count_words(sentence)
+puts "The number of words in the sentence is: #{word_count}"
