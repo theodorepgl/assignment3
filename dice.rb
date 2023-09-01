cd6c1b5bc544fee14d742489bcfb889692503220
theodorepgl <limlimtheo96@gmail.com>
2023-09-01 11:10:00 +0800

    Dice done

--- dice.rb
+++ dice.rb
@@ -1,1 +1,11 @@
 #Build a class Dice with a method to roll the dice and simulate a random number from 1 to /6.
+
+class Dice
+  def roll
+    rand(1..6)
+  end
+end
+
+dice = Dice.new
+random_number = dice.roll
+puts "The dice rolled: #{random_number}"
