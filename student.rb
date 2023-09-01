d9402dde5ae5100f10f1017b5342cb12bbba5f61
theodorepgl <limlimtheo96@gmail.com>
2023-09-01 15:11:36 +0800

    50 50

--- student.rb
+++ student.rb
@@ -1,2 +1,20 @@
 # Create a class Student with attributes name and grades as an array. 
 # Implement a method in the Student class to calculate the average grade.
+
+class Student
+  attr_accessor :name, :grades
+  
+  def initialize(name, grades)
+    @name = name
+    @grades = grades
+  end
+  
+  def average_grade
+    total_grades = @grades.reduce(0, :+)
+    average = total_grades.to_f / @grades.length
+    average.round(2) # rounding to 2 decimal places
+  end
+end
+
+student = Student.new("Theodore", [84, 96, 90, 87, 93])
+puts student.average_grade
