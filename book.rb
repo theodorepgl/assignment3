bb0d9889c2128b6aa2ae74c777aba20dccbcd518
theodorepgl <limlimtheo96@gmail.com>
2023-09-01 11:20:42 +0800

    Book done

--- book.rb
+++ book.rb
@@ -1,2 +1,25 @@
 #Create a class Book with attributes title, author, and pages. 
 #Add a method to display the book's information.
+require 'colorize'
+
+class Book
+  attr_accessor :title, :author, :pages
+  
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+  
+  def display_book
+    puts "Title: #{@title}".light_blue
+    puts "Author: #{@author}".light_cyan
+    puts "Pages: #{@pages}".light_magenta
+  end
+end
+
+#Book class
+book_type = Book.new("Rich Dad Poor Dad", "Robert Kiyosaki and Sharon L. Lechter", 336)
+
+#Display
+book_type.display_book
