4c642d96e2c013e6517f90e6de745605ab479c8f
theodorepgl <limlimtheo96@gmail.com>
2023-09-01 11:09:42 +0800

    50 50

--- circle.rb
+++ circle.rb
@@ -1,1 +1,22 @@
 #Develop a class Circle with attributes radius and methods to calculate the area and circumference.
+
+class Circle
+  attr_accessor :radius
+
+  def initialize(radius)
+    @radius = radius
+  end
+
+  def area
+    Math::PI * @radius**2
+  end
+
+  def circumference
+    2 * Math::PI * @radius
+  end
+end
+
+# Example usage:
+circle = Circle.new(5)
+puts "Area: #{circle.area}"
+puts "Circumference: #{circle.circumference}"
