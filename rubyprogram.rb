382f45703f0f04f11324923b1c57b9292de2d715
theodorepgl <limlimtheo96@gmail.com>
2023-09-03 22:22:40 +0800

    15.Final

--- rubyprogram.rb
+++ rubyprogram.rb
@@ -1,4 +1,5 @@
 # Write a Ruby program that takes a sentence as input and outputs the number of words in it.
+require 'colorize'
 
 def count_words(sentence)
   words = sentence.split(" ")
@@ -9,4 +10,4 @@ print "Enter a sentence: "
 sentence = gets.chomp
 
 word_count = count_words(sentence)
-puts "The number of words in the sentence is: #{word_count}"
+puts "The number of words in the sentence is: #{word_count}".light_cyan
