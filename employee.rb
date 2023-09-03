7102a2626c015f726189294bd946c14d3037846d
theodorepgl <limlimtheo96@gmail.com>
2023-09-03 22:02:17 +0800

    9.Final

--- employee.rb
+++ employee.rb
@@ -1,5 +1,6 @@
 # Implement a class Employee with attributes name, position, and salary. 
 # Add a method to give a raise to the employee's salary.
+require 'colorize'
 
 class Employee
   attr_accessor :name, :position, :salary
@@ -10,15 +11,17 @@ class Employee
     @salary = salary
   end
 
-  def give_raise(amount)
+  def raise(amount)
     @salary += amount
   end
 end
 
-employee = Employee.new("Theodore", "Engineer", 4000)
-puts "Current salary: $#{employee.salary}"
-employee.give_raise(1000)
-puts "New salary after raise: $#{employee.salary}"
-
-Current salary: $4000
-New salary after raise: $5000
+employee1 = Employee.new("Theodore", "Engineer", 4000)
+puts "#{employee1.position} #{employee1.name} Current salary: $#{employee1.salary}".light_yellow
+employee1.raise(1000)
+puts "New salary after raise: $#{employee1.salary}".light_yellow
+puts
+employee2 = Employee.new("Dennis", "CEO", 8000)
+puts "#{employee2.position} #{employee2.name} Current salary: $#{employee2.salary}".light_cyan
+employee2.raise(2000)
+puts "New salary after raise: $#{employee2.salary}".light_cyan
