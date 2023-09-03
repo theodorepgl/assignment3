846e3b687c8239ce051d598d4310b374f642203c
theodorepgl <limlimtheo96@gmail.com>
2023-09-03 21:36:00 +0800

    3.Final

--- dice.rb
+++ dice.rb
@@ -1,4 +1,5 @@
 #Build a class Dice with a method to roll the dice and simulate a random number from 1 to /6.
+require 'colorize'
 
 class Dice
   def roll
@@ -7,5 +8,6 @@ class Dice
 end
 
 dice = Dice.new
+
 random_number = dice.roll
-puts "The dice rolled: #{random_number}"
+puts "The dice rolled: #{random_number}".light_yellow
